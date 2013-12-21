39b5d4b7019ad02460b6b5c88413c8c8fc89df6a
Arcao <arcao@arcao.com>
2013-12-21 10:17:17 +0100

    Added Place POJO

---
+++ src/main/java/com/arcao/menza/api/data/Place.java
@@ -0,0 +1,10 @@
+package com.arcao.menza.api.data;
+
+/**
+ * Created by msloup on 10.11.13.
+ */
+public class Place {
+	public String name;
+	public String description;
+	public Image[] images;
+}
