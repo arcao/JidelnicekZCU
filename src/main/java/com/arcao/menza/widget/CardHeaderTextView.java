a02f3899a98b975d4d5df2012e32b35d4da49c7e
Arcao <arcao@arcao.com>
2014-03-17 08:09:02 +0100

    Margin and padding is now applied correctly in preview windows Changed card header text to use Roboto-Light font.

---
+++ src/main/java/com/arcao/menza/widget/CardHeaderTextView.java
@@ -0,0 +1,33 @@
+package com.arcao.menza.widget;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CardHeaderTextView extends TextView {
+	protected static Typeface FONT = null;
+
+	public CardHeaderTextView(Context context) {
+		super(context);
+		init(this, context);
+	}
+
+	public CardHeaderTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(this, context);
+	}
+
+	public CardHeaderTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(this, context);
+	}
+
+	protected void init(CardHeaderTextView textView, Context context) {
+		if (isInEditMode()) return;
+
+		if (FONT == null)
+			FONT = Typeface.createFromAsset(context.getAssets(), "Roboto-Light.ttf");
+		textView.setTypeface(FONT);
+	}
+}
