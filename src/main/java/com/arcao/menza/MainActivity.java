1fef9f4c878b8d15e440cc925d812cfa349124af
Arcao <arcao@arcao.com>
2015-06-01 22:13:12 +0200

    Switching to NavigationView from design library for NavigationDrawer

--- src/main/java/com/arcao/menza/MainActivity.java
+++ src/main/java/com/arcao/menza/MainActivity.java
@@ -4,26 +4,32 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.arcao.menza.adapter.DayPagerAdapter;
 import com.arcao.menza.constant.AppConstant;
 import com.arcao.menza.constant.PrefConstant;
-import com.arcao.menza.fragment.NavigationDrawerFragment;
 import com.arcao.menza.fragment.dialog.PriceGroupChangeableDialogFragment;
 import com.arcao.menza.fragment.dialog.PriceGroupSelectionDialogFragment;
 import com.arcao.menza.util.FeedbackHelper;
 
-public class MainActivity extends AbstractBaseActivity implements PriceGroupSelectionDialogFragment.OnPriceGroupSelectedListener, NavigationDrawerFragment.OnDrawerCallbackListener {
+public class MainActivity extends AbstractBaseActivity implements PriceGroupSelectionDialogFragment.OnPriceGroupSelectedListener {
 	public static final String PARAM_PLACE_ID = "PLACE_ID";
   private static final String STATE_PLACE_ID = "STATE_PLACE_ID";
 	public static final int RESULT_REFRESH = 101;
 
-	private NavigationDrawerFragment mNavigationDrawerFragment;
+	private NavigationView mNavigationView;
+	private DrawerLayout mDrawerLayout;
 	private DayPagerAdapter mDayPagerAdapter;
 	private ViewPager mViewPager;
 	private SharedPreferences mSharedPreferences;
@@ -45,14 +51,37 @@ public class MainActivity extends AbstractBaseActivity implements PriceGroupSele
 		setContentView(R.layout.activity_main);
 
 		// prepare toolbar
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
+		final ActionBar ab = getSupportActionBar();
+		ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+		ab.setDisplayHomeAsUpEnabled(true);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 		// prepare drawer
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
-		mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), getToolbar());
-		mNavigationDrawerFragment.setPlaceId(placeId);
-		setTitle(mNavigationDrawerFragment.getPlaceName(placeId));
+		mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+		if (mNavigationView != null) {
+			mNavigationView.setNavigationItemSelectedListener(
+							new NavigationView.OnNavigationItemSelectedListener() {
+								@Override
+								public boolean onNavigationItemSelected(MenuItem menuItem) {
+									if (menuItem.getGroupId() == R.id.place) {
+										onPlaceSelected(menuItem);
+									} else {
+										switch (menuItem.getItemId()) {
+											case R.id.action_settings:
+												onSettingsSelected();
+												break;
+											case R.id.action_feedback:
+												onFeedbackSelected();
+												break;
+										}
+									}
+
+									mDrawerLayout.closeDrawers();
+									return true;
+								}
+							});
+		}
 
 		// prepare day adapter
 		mDayPagerAdapter = new DayPagerAdapter(this, getSupportFragmentManager(), placeId);
@@ -62,6 +91,43 @@ public class MainActivity extends AbstractBaseActivity implements PriceGroupSele
 
 		PagerTitleStrip pagerTitleStrip = (PagerTitleStrip) findViewById(R.id.pager_title_strip);
 		pagerTitleStrip.setNonPrimaryAlpha(0.3F); // in percent
+
+		MenuItem menuItem = getMenuItemInGroup(mNavigationView.getMenu(), R.id.place, placeId);
+		if (menuItem != null) {
+			onPlaceSelected(menuItem);
+		}
+	}
+
+	@Nullable
+	private MenuItem getMenuItemInGroup(@NonNull Menu menu, int groupId, int index) {
+		int indexInGroup = 0;
+
+		for (int i = 0; i < menu.size(); i++) {
+			MenuItem menuItem = menu.getItem(i);
+
+			if (menuItem.getGroupId() == groupId) {
+				if (indexInGroup == index) {
+					return menuItem;
+				}
+				indexInGroup++;
+			}
+		}
+
+		return null;
+	}
+
+	private int getMenuIndexInGroup(@NonNull Menu menu, @NonNull MenuItem menuItem) {
+		int index = 0;
+		for (int i = 0; i < menu.size(); i++) {
+			MenuItem currMenuItem = menu.getItem(i);
+			if (currMenuItem.getGroupId() == menuItem.getGroupId()) {
+				if (menuItem.equals(currMenuItem))
+					return index;
+				index++;
+			}
+		}
+
+		return -1;
 	}
 
 	@Override
@@ -90,6 +156,9 @@ public class MainActivity extends AbstractBaseActivity implements PriceGroupSele
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+			case android.R.id.home:
+				mDrawerLayout.openDrawer(GravityCompat.START);
+				return true;
 			case R.id.action_today:
 				mViewPager.setCurrentItem(AppConstant.DAY_ID_TODAY, true);
 				return true;
@@ -101,12 +170,10 @@ public class MainActivity extends AbstractBaseActivity implements PriceGroupSele
 		}
 	}
 
-	@Override
 	public void onSettingsSelected() {
 		startActivityForResult(new Intent(this, SettingsActivity.class), 0);
 	}
 
-	@Override
 	public void onFeedbackSelected() {
 		FeedbackHelper.sendFeedBack(this, R.string.feedback_email, R.string.feedback_subject, R.string.feedback_message, false);
 	}
@@ -129,10 +196,11 @@ public class MainActivity extends AbstractBaseActivity implements PriceGroupSele
 		}
 	}
 
-	@Override
-	public void onPlaceSelected(int placeId) {
-		this.placeId = placeId;
-		setTitle(mNavigationDrawerFragment.getPlaceName(placeId));
+	public void onPlaceSelected(MenuItem menuItem) {
+		placeId = getMenuIndexInGroup(mNavigationView.getMenu(), menuItem);
+		menuItem.setChecked(true);
+		setTitle(menuItem.getTitle());
+		((TextView)mNavigationView.findViewById(R.id.title)).setText(menuItem.getTitle());
 		mDayPagerAdapter.updatePlaceId(placeId);
 	}
 }
