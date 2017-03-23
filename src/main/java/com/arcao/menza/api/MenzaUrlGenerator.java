be8285ab66c599066d8111fa12bfe395f19b8d30
Arcao <arcao@arcao.com>
2017-03-23 10:50:39 +0100

    Switch to https protocol

--- src/main/java/com/arcao/menza/api/MenzaUrlGenerator.java
+++ src/main/java/com/arcao/menza/api/MenzaUrlGenerator.java
@@ -9,7 +9,7 @@ import timber.log.Timber;
 public class MenzaUrlGenerator {
     private static final SimpleDateFormat DAY_FORMAT = new SimpleDateFormat("yyyyMMdd", Locale.US);
 
-    private static final String URL_PREFIX = "http://menza.arcao.com/api/v2.1/";
+    private static final String URL_PREFIX = "https://menza.arcao.com/api/v2.1/";
     private static final String DAY_URL = URL_PREFIX + "get/%d/%s";
     private static final String RATING_URL = URL_PREFIX + "vote/";
     private static final String PLACE_URL = URL_PREFIX + "places/%d/";
